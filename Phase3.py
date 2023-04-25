b401602f32951eb9ee40867311272bd48f519baa
mohak-borole <mohakborole@gmail.com>
2023-04-25 22:41:43 +0530

    extra datasets

--- Phase3.py
+++ Phase3.py
@@ -10,7 +10,7 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 id = 0
 
 # names related to ids: example ==> Marcelo: id=1,  etc
-names = ['None', 'Mohak' , 'Chinmay' , 'Omkar' , 'Mahadev' , 'Chinmay' ,'Mohak' , 'Mahadev' , 'Omkar' , 'Siddhant' , 'Siddhant' ] 
+names = ['None', 'Mohak' , 'Chinmay' , 'Omkar' , 'Mahadev' , 'Chinmay' ,'Mohak' , 'Mahadev' , 'Omkar' , 'Siddhant' , 'Siddhant', 'Deep' ] 
 
 # Initialize and start realtime video capture
 cam = cv2.VideoCapture(0)
