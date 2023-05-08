19ba164f35af1e6e92219bfb9df93faf600eecdb
OmkarAher45687 <113294166+OmkarAher45687@users.noreply.github.com>
2023-05-08 13:35:12 +0530

    Update GUI Vr3.py

--- GUI Vr3.py
+++ GUI Vr3.py
@@ -41,10 +41,10 @@ def make_something(value):
     global variable
     variable = value
 
-b3=Button(fg="white",text="register person",width=20,bg="green")
-b3.grid(row=3,column=5)
-b4=Button(fg="white",text="remove person",width=20,bg="red")
-b4.grid(row=5,column=5)
+#b3=Button(fg="white",text="register person",width=20,bg="green")
+#b3.grid(row=3,column=5)
+#b4=Button(fg="white",text="remove person",width=20,bg="red")
+#b4.grid(row=5,column=5)
 
 i=1
 for visitor in my_conn: 
